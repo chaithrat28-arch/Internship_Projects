90605a916968860241d2b72a6b2fffe4b5bb5f4b
chaithrat28-arch <chaithrat28@gmail.com>
2026-03-22 12:37:53 +0530

    Create README.h

---
+++ AddressBook/README.h
@@ -0,0 +1,33 @@
+📒 Address Book
+
+📌 Description
+A modular Address Book application in C that allows users to manage contacts efficiently using file handling. The project is structured into multiple source and header files for better readability and maintainability.
+
+🚀 Features
+* Add new contacts
+* Search existing contacts
+* Edit contact details
+* Delete contacts
+* Display all contacts
+* Persistent storage using files
+
+🛠️ Tech Stack
+* Language: C
+* Concepts:Structures, File Handling, Modular Programming
+
+📂 Project Structure
+bash
+AddressBook/
+│── main.c
+│── contact.c / contact.h
+│── edit.c / edit.h
+│── delete.c / delete.h
+│── file.c / file.h
+
+▶️ How to Run
+bash
+gcc main.c contact.c edit.c delete.c file.c -o addressbook
+./addressbook
+
+💡 Use Case
+Simulates a basic **contact management system** similar to phone address books.
